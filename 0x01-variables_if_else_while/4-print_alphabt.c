8e6a74b297bcd57f1c1fdd6773cfed8ef0ecd8c0
ChibweMusonda <reubenchibwe@gmail.com>
2022-09-10 04:44:28 -0700

    commit update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point in the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')	
+{
+	putchar(ch);
+}
+}
+putchar('⧵n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point in the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("⧵n");
+return (0);
+}
