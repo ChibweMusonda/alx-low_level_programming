fa2d049fa2e0c17dd798eb4c8e0087bf46b30e8b
root <root@DESKTOP-JI669HD.localdomain>
2022-09-13 23:22:28 +0200

    9-times_table.c updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,18 +7,16 @@
  */
 void print_to_98(int n)
 {
-if (n >= 98)
-{
-while (n > 98)
-
-	printf("%d, ", n--);
+	if (n >= 98)
+	{
+	while (n > 98)
+		printf("%d, ", n--);
 	printf("%d\n", n);
-}
-else
-{
-while (n < 98)
-
-	printf("%d, ", n++);
+	}
+	else
+	{
+	while (n < 98)
+		printf("%d, ", n++);
 	printf("%d\n", n);
-}
+	}
 }
