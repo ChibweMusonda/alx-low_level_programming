ba9d7ab25497d8e76cd59a38f43cc781d02bc4c2
root <root@DESKTOP-JI669HD.localdomain>
2022-09-21 20:52:11 +0200

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed
+ * @dest: string that will be pointed
+ * @src: string to be concatenated upon
+ * retutn: pointer to @dest
+ */
+char *_strcat(char *dest, char *src);
+{
+int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
