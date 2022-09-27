cd0a2391d9c268a5f9c83e9d1869e9349131e1e3
root <root@DESKTOP-JI669HD.localdomain>
2022-09-27 04:39:33 +0200

    update for my first task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n);
 {
-	while (n)
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
 	{
-		s[n - 1] = b;
-		n--;
+		*(s + x) = b;
 	}
 	return (s);
+
 }
 
